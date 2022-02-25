d2bb8916cc4e496e76b7aef06428abaa6f412be3
Jacob Quant <jacobq@gmail.com>
2022-02-25 14:59:52 -0600

    demonstrate ember-did-resize incompatibility with ember 4.x

---
+++ app/controllers/application.js
@@ -0,0 +1,31 @@
+import { tracked } from '@glimmer/tracking';
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+class Item {
+  @tracked itemContent;
+
+  constructor(itemContent) {
+    this.itemContent = itemContent;
+  }
+
+  toString() {
+    return this.itemContent;
+  }
+}
+
+export default class ApplicationController extends Controller {
+  items = [
+    new Item('Try resizing to add more items')
+  ];
+
+  #itemNumber = 1;
+
+  @action
+  addItem() {
+    const message = `${Date.now()}: Item #${this.#itemNumber}`
+    this.items.pushObject(new Item(message));
+    console.log('addItem called', message);
+    this.#itemNumber++;
+  }
+}
